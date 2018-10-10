4ce48e3119b4cc5077ef7a72bf077ac0444393cf
Xerus <27jf@web.de>
2018-10-10 18:05:11 +0200

    React

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+
+interface State {
+}
+
+
+export class App extends React.Component<any, State> {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        This is a test application!
+      </div>
+    )
+  }
+}
